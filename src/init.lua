26290449899e7cbba8c3b59a6e038a9ef7601a72
Twin1Twin2 <theawesometwin827@gmail.com>
2018-08-01 06:03:46 -0700

    Added Resource to Framework

--- src/init.lua
+++ src/init.lua
@@ -4,17 +4,19 @@ local ECSComponentDescription = require(script.ECSComponentDescription)
 local ECSEngine = require(script.ECSEngine)
 local ECSEngineConfiguration = require(script.ECSEngineConfiguration)
 local ECSEntity = require(script.ECSEntity)
+local ECSRobloxResource = require(script.ECSRobloxResource)
 local ECSSystem = require(script.ECSSystem)
 local ECSWorld = require(script.ECSWorld)
 
 
 local ECSFramework = {}
 
+ECSFramework.Component = ECSComponentDescription
 ECSFramework.Engine = ECSEngine
 ECSFramework.EngineConfiguration = ECSEngineConfiguration
-ECSFramework.World = ECSWorld
-ECSFramework.Component = ECSComponentDescription
+ECSFramework.Resource = ECSRobloxResource
 ECSFramework.System = ECSSystem
+ECSFramework.World = ECSWorld
 
 
 return ECSFramework
